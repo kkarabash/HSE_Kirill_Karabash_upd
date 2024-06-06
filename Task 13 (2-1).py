e08352da103f2129f6fe4085bfd8bd91c4023e51
Kirill Karabash <150274189+kkarabash@users.noreply.github.com>
2024-06-06 20:14:31 +0300

    new version

--- Task 13 (2-1).py
+++ Task 13 (2-1).py
@@ -1,14 +1,7 @@
-def two_sum(nums, target):
-    num_to_index = {}
-    for index, num in enumerate(nums):
-        complement = target - num
-        if complement in num_to_index:
-            return [num_to_index[complement], index]
-        num_to_index[num] = index
-    return None
+def is_palindrome(number: int) -> bool:
+    # Преобразуем число в строку
+    num_str = str(number)
+    # Проверяем, равно ли строковое представление числа его обратному
+    return num_str == num_str[::-1]
+print(is_palindrome(127))
 
-# Пример:
-nums = [2, 7, 11, 15]
-target = 9
-result = two_sum(nums, target)
-print(result)
